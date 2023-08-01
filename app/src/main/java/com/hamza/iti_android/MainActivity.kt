00bc53952bf739a39a33736d71790e74694a5402
hamza <aheita3@gmail.com>
2023-08-01 15:12:43 +0300

    lab 1

--- app/src/main/java/com/hamza/iti_android/MainActivity.kt
+++ app/src/main/java/com/hamza/iti_android/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.hamza.iti_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hamza.iti_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding?.root)
+        init()
+
+    }
+
+    private fun init() {
+
+        binding.btnOk.setOnClickListener {
+            if (binding.edtText.text.toString().isNotEmpty()) {
+
+                binding.txtName.text = "Your Name is " + binding.edtText.text.toString()
+
+            } else {
+                binding.edtText.error = "Required"
+
+            }
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
+
