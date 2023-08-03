a01fd0512cd5cc7da3e9354a2bf5507bb11c0b41
hamza <aheita3@gmail.com>
2023-08-03 17:42:04 +0300

    lab 3

--- app/src/main/java/com/hamza/iti_android/MainActivity.kt
+++ app/src/main/java/com/hamza/iti_android/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.hamza.iti_android
 
-import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.content.res.Resources
 import android.os.Bundle
-import android.widget.RadioButton
+import android.view.View
+import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
 import com.hamza.iti_android.databinding.ActivityMainBinding
 import com.hamza.iti_android.utils.showToast
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-    private var arr = arrayListOf<String>()
+    private var arrGender = arrayListOf<String>()
+    private var arrHobbies = arrayListOf<String>()
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
@@ -19,7 +20,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding?.root)
-
+        arrHobbies.clear()
+        arrGender
         actions()
         showingToast()
 
@@ -27,46 +29,73 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showingToast() {
+       // manageInputs()
         manageCheckBox()
         manageRadioButton()
-        for (item in arr){
-            showToast(item)
+
+        showToast("Your userName is ${binding.edtUsername.text.toString()} \n ${arrHobbies.joinToString()} " +
+                "\n ${arrGender.joinToString()}")
+
+
+    }
+
+    private fun manageInputs() {
+        val text = binding.edtUsername.text
+
+        if (text.equals(null)) {
+          //  arr.add("Your username is ${text}\n")
         }
     }
 
-    private fun manageRadioButton() {
-        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
-            val nameOfCheckId = findViewById<RadioButton>(checkedId)
-            val name = nameOfCheckId.text.toString()
-            arr.add(name)
 
+    private fun manageRadioButton() {
+        binding.maleCheck.setOnCheckedChangeListener { _, b ->
+            if (b) {
+                binding.femaleCheck.isChecked = false
+
+                arrGender.add("Your gender is ${binding.maleCheck.text.toString()}\n")
+
+            }
         }
+
+        binding.femaleCheck.setOnCheckedChangeListener { _, b ->
+            if (b) {
+                binding.maleCheck.isEnabled = false
+                arrGender.add("Your gender is ${binding.maleCheck.text.toString()}\n")
+
+
+            }
+        }
+
     }
 
     private fun manageCheckBox() {
-           var msgChcekBox: String? = null
+
+
         binding.apply {
             sport1.setOnCheckedChangeListener { buttonView, isChecked ->
                 if (isChecked) {
-                    arr.add(binding.sport1.text.toString())
+                    arrHobbies.add(binding.sport1.text.toString())
+
                 }
 
             }
             sport2.setOnCheckedChangeListener { buttonView, isChecked ->
                 if (isChecked) {
-                    arr.add(binding.sport2.text.toString())
-                }
+                    arrHobbies.add(binding.sport2.text.toString())
 
-            }
-            sport3.setOnCheckedChangeListener { buttonView, isChecked ->
-                if (isChecked) {
-                    arr.add(binding.sport2.text.toString())
-                }
 
+                }
+                sport3.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) {
+                        arrHobbies.add(binding.sport2.text.toString())
+
+                    }
+
+                }
             }
         }
     }
-
 
     private fun actions() {
         binding.apply {
@@ -77,8 +106,11 @@ class MainActivity : AppCompatActivity() {
                 setLocale(targetLocale)
                 recreate()
             }
+
             btnLogin.setOnClickListener {
+
                 showingToast()
+
             }
         }
 
--- app/src/main/java/com/hamza/iti_android/utils/Extension.kt
+++ app/src/main/java/com/hamza/iti_android/utils/Extension.kt
@@ -6,6 +6,6 @@ import com.hamza.iti_android.R
 import io.github.muddz.styleabletoast.StyleableToast
 
 
-fun Context.showToast(message: Any, duration: Int = Toast.LENGTH_SHORT) {
+fun Context.showToast(message: Any, duration: Int = Toast.LENGTH_LONG) {
     StyleableToast.makeText(this, message.toString(), duration, R.style.toastStyle).show();
 }
